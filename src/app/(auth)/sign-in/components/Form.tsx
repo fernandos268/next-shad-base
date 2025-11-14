4b5229dec1fb7f6e9b23a1a32123e3974d6f85f8
Fern <singsonfernandojr@gmail.com>
2025-11-14 19:04:52 +0800

    add Facebook social signin method

--- src/app/(auth)/sign-in/components/Form.tsx
+++ src/app/(auth)/sign-in/components/Form.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { toast } from "sonner"
 import { Toaster } from "@/components/ui/sonner"
-import { signInAction, authenticateWithGoogleAction } from '@/app/(auth)/actions/auth'
+import { signInAction } from '@/app/(auth)/actions/auth'
 import { PasswordInput } from "@/components/composed/PasswordInput"
 import { Button } from "@/components/ui/button"
 import {
@@ -20,7 +20,7 @@ import { useForm, Controller } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 // import { ClassProp } from "class-variance-authority/types"
 import { Spinner } from '@/components/ui/spinner'
-
+import SocialSigninButon from '@/components/composed/SocialSigninButon'
 
 const defaultFieldValues: UserInput = {
   email: '',
@@ -57,22 +57,17 @@ const Form = () => {
           <div className="flex flex-col items-center gap-1 text-center">
             <h1 className="text-2xl font-bold">Welcome!</h1>
             <p className="text-muted-foreground text-sm text-balance">
-              Login with your Google account
+              Login with your social account
             </p>
           </div>
-          <Field>
-            <Button variant="outline" type="button" size='lg' asChild>
-              <Link href="/auth/login?connection=google-oauth2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                  <path
-                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                    fill="currentColor"
-                  />
-                </svg>
-                Sign in with Google
-              </Link>
-            </Button>
-          </Field>
+          <div className="flex flex-col gap-3">
+            <Field>
+              <SocialSigninButon href='/auth/login?connection=google-oauth2&returnTo=/profile' type='google' />
+            </Field>
+            <Field>
+              <SocialSigninButon href='/auth/login?connection=facebook&returnTo=/profile' type='facebook' />
+            </Field>
+          </div>
 
           <FieldSeparator className="bg-transparent">Or continue with</FieldSeparator>
 
--- src/components/ui/field.tsx
+++ src/components/ui/field.tsx
@@ -173,7 +173,7 @@ function FieldSeparator({
       <Separator className="absolute inset-0 top-1/2" />
       {children && (
         <span
-          className=" text-muted-foreground relative mx-auto block w-fit px-2"
+          className=" text-muted-foreground relative mx-auto block w-fit px-2 bg-background"
           data-slot="field-separator-content"
         >
           {children}
